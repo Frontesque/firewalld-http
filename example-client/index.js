cfab3c27b3c57a5b274b401a49b640a9cffe7ac5
= <=>
2024-11-15 20:42:35 -0500

    working data parsing

--- example-client/index.js
+++ example-client/index.js
@@ -1,7 +1,14 @@
 const axios = require('axios');
 
 async function main() {
-    const response = await axios.get('http://localhost:9988/');
+    const response = await axios.post('http://localhost:9988/', {
+        firstName: 'Fred',
+        lastName: 'Flintstone'
+    }, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+    });
     console.log(response.data);
 }
 main();
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 //---   Imports   ---//
-use rouille::{ router, Response };
+use rouille::{ router, Response, try_or_400, post_input };
+// use serde_json::{ from_str, Value};
 
 //---   Global Variables   ---//
 const LISTEN_ON: &str = "0.0.0.0:9988";
 
 //---   Start Server   ---//
 fn main() {
+    println!("FirewallD-http is listening at: {}", LISTEN_ON);
+    println!("!!!   IMPORTANT NOTICE   !!!");
+    println!("Ensure that this port is only able to accessed by trusted applications");
+    println!("Any program or user that can access {} is able to change your firewall configuration.", LISTEN_ON);
+    println!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     rouille::start_server(LISTEN_ON, move |request| {
         router!(request,
             (POST) (/) => {
+                let raw_data = try_or_400!(post_input!(request, {
+                    firstName: String,
+                    lastName: String,
+                }));
+                println!("{:?}", raw_data);
                 Response::text("hello world")
             },
             (GET) (/) => {
